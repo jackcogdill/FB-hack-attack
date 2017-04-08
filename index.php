3394199646cab678a0cff1856d84706f340b17d8
Jack Cogdill <jackcogdill@users.noreply.github.com>
2017-04-08 14:04:04 -0400

    Merged Richard's code

--- index.php
+++ index.php
@@ -2,10 +2,26 @@
 
 require_once("secure.php");
 require_once("head_top.php");
+?>
 
+<link rel="stylesheet" href="/css/home.css">
+
+<?php
 require_once("head_bottom.php");
 require_once("header.php");
+?>
 
+<h3 class="center">Languages</h3>
+<ul id="button-wrap">
+	<li><a class="java button" href="">Java</a></li>
+	<li><a class="python button" href="/challenge">Python</a></li>
+	<li><a class="c button" href="">C</a></li>
+	<li><a class="js button" href="">Javascript</a></li>
+	<li><a class="ruby button" href="">Ruby</a></li>
+	<li><a class="php button" href="">PHP</a></li>
+</ul>
+
+<?php
 require_once("footer.php");
 
 ?>
