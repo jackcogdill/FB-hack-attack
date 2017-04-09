7c32c644ec6e969c0a3faece55b4f4b397173629
Jack Cogdill <jackcogdill@users.noreply.github.com>
2017-04-08 22:12:08 -0400

    Fix secure.php

--- secure.php
+++ secure.php
@@ -1,11 +1,15 @@
 <?php
 
-require_once("db.php");
+if (!isset($up)) {
+	$up = '';
+}
+
+require_once($up . "db.php");
 
 // Check if user is logged in
 if (empty($_SESSION['user'])) {
 	// If not, redirect to login page
-	header("Location: login");
+	header("Location: ".$up."login");
 	
 	// Must die
 	die("Redirecting");
