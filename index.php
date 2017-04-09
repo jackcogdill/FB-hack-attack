c43ee947f4a3ebbe544cc549e20d6cc32c18e61c
Jack Cogdill <jackcogdill@users.noreply.github.com>
2017-04-09 11:59:25 -0400

    Added 4th challenge

--- index.php
+++ index.php
@@ -40,6 +40,7 @@ if (isset($_SESSION['user']['hash_id'])) {
 	<option value="1">1</option>
 	<option value="2">2</option>
 	<option value="3">3</option>
+	<option value="4">4</option>
 </select>
 <button type="submit" name="language" class="python" value="Python"></button></li>
 	<li>C<button type="submit" name="language" class="c" value="C"></button></li>
