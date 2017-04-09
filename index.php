214f9c3af584c0f3086c35bda6d79b078a302505
Jack Cogdill <jackcogdill@users.noreply.github.com>
2017-04-09 09:18:52 -0400

    Delete session and rows in table

--- index.php
+++ index.php
@@ -9,18 +9,37 @@ require_once("head_top.php");
 <?php
 require_once("head_bottom.php");
 require_once("header.php");
+
+// Reset any matche
+if (isset($_SESSION['user']['hash_id'])) {
+	$id = $_SESSION['user']['hash_id'];
+	$select = "
+		DELETE FROM ongoing
+		WHERE id = '{$id}'
+		LIMIT 1
+	";
+	$query = mysqli_query($connect, $select);
+
+	if (isset($_SESSION['user']['hash_id'])) {
+		unset($_SESSION['user']['hash_id']);
+	}
+	if (isset($_SESSION['user']['waiting'])) {
+		unset($_SESSION['user']['waiting']);
+	}
+}
+
 ?>
 
 <p id="logo"> Hack Attack </p>
 <hr class="underline">
 <form action="challenge/index.php" method="post">
 <ul id="button-wrap">
-	<li><button type="submit" name="language" class="java" value="Java">Java</button></li>
-	<li><button type="submit" name="language" class="python" value="Python">Python</button></li>
-	<li><button type="submit" name="language" class="c" value="C">C</button></li>
-	<li><button type="submit" name="language" class="js" value="Javascript">Javascript</button></li>
-	<li><button type="submit" name="language" class="ruby" value="Ruby">Ruby</button></li>
-	<li><button type="submit" name="language" class="php" value="PHP">PHP</button></li>
+	<li>Java<button type="submit" name="language" class="java" value="Java"></button></li>
+	<li>Python<button type="submit" name="language" class="python" value="Python"></button></li>
+	<li>C<button type="submit" name="language" class="c" value="C"></button></li>
+	<li>Javascript<button type="submit" name="language" class="js" value="Javascript"></button></li>
+	<li>Ruby<button type="submit" name="language" class="ruby" value="Ruby"></button></li>
+	<li>PHP<button type="submit" name="language" class="php" value="PHP"></button></li>
 </ul>
 </form>
 
