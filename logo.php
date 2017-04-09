c056391c38750b6500ae44570b8ef6ca4b7be268
Kyle Long <kylel95@vt.edu>
2017-04-09 02:34:25 -0400

    Add logo p tag

---
+++ logo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+	<style type="text/css">
+		p
+		{
+			position: absolute;
+			top:100px;
+			left:450px;
+			color:green;
+			font-size: 30px;
+			 font-family: 'Press Start 2P', cursive;
+		}
+	</style>
+</head>
+<body>
+<p> Hack Attack </p>
+</body>
+</html>
