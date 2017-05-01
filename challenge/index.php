ec487792d49249b1a7ecdb5112cddd2ad3dc08d6
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2017-05-01 16:53:58 -0400

    added spinner

--- challenge/index.php
+++ challenge/index.php
@@ -674,7 +674,7 @@ if ($waiting_flag) {
 ?>
 
 <style>
-img.loading {
+.loading {
 	margin: 10% auto 0;
 	display: block;
 }
@@ -687,7 +687,7 @@ div.desc {
 }
 </style>
 
-<img src='../images/loading.gif' class='loading'/>
+<div id="spinner" class="loading"></div>
 <div class='desc'>
 <?php
 if (!empty($want_opponent)) {
@@ -699,6 +699,34 @@ else {
 ?>
 </div>
 
+<script src="../js/spin.min.js"></script>
+<script type="text/javascript">
+var opts = {
+  lines: 13 // The number of lines to draw
+, length: 28 // The length of each line
+, width: 14 // The line thickness
+, radius: 42 // The radius of the inner circle
+, scale: 1 // Scales overall size of the spinner
+, corners: 1 // Corner roundness (0..1)
+, color: '#FFF' // #rgb or #rrggbb or array of colors
+, opacity: 0.3 // Opacity of the lines
+, rotate: 0 // The rotation offset
+, direction: 1 // 1: clockwise, -1: counterclockwise
+, speed: 1 // Rounds per second
+, trail: 60 // Afterglow percentage
+, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+, zIndex: 2e9 // The z-index (defaults to 2000000000)
+, className: 'spinner' // The CSS class to assign to the spinner
+, top: '50%' // Top position relative to parent
+, left: '50%' // Left position relative to parent
+, shadow: false // Whether to render a shadow
+, hwaccel: false // Whether to use hardware acceleration
+, position: 'absolute' // Element positioning
+}
+var target = document.getElementById('spinner');
+// console.log(target);
+var spinner = new Spinner(opts).spin(target);
+</script>
 <script type="text/javascript">
 function redirect() {
 	var xhr = new XMLHttpRequest();
