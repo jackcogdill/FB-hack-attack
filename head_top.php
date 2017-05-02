261fb7504079bad8b5f8dab63f0306ec416c9b78
Jack Cogdill <jackcogdill@users.noreply.github.com>
2017-05-02 11:28:10 -0400

    Added title

--- head_top.php
+++ head_top.php
@@ -1,4 +1,5 @@
 <!doctype html>
 <html><head>
+<title>Hack Attack</title>
 <link rel="stylesheet" href=<?php echo $up . "css/main.css"; ?>>
 <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
