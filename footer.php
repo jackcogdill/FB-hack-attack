d5aab6a65b92fd84a6b56fa0a195f1ddc296dc37
Jack Cogdill <jackcogdill@users.noreply.github.com>
2017-05-02 00:26:40 -0400

    Fixed messed up footer

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
-	<div id="footer">Check out the source code on: <a href="https://github.com/jackcogdill/FB-hack-attack">Github</a></div>
 </div>
+<div id="footer">Check out the source code on: <a href="https://github.com/jackcogdill/FB-hack-attack">Github</a></div>
 </body></html>
